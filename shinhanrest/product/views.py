6ba2996182418d576f78ea5095e319987d610a5e
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-18 11:20:22 +0900

    feat: Add send data to Postman feature

---
+++ shinhanrest/product/views.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ProductTempView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response({"temp": 1})
