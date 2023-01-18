f43940a064791418b6c9221cbeca11b721743e40
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-18 13:46:11 +0900

    feat: Add product get features in product/

--- shinhanrest/product/urls.py
+++ shinhanrest/product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductTempView.as_view()),
-]
+    path("/<int:pk>", views.ProductDetailView.as_view()),
+    path("/", views.ProductListView.as_view()),
+]   
--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -1,8 +1,38 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Product
 
+# CRUD
+# Create Read Update Delete
 
-class ProductTempView(APIView):
+class ProductListView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        pass
 
     def get(self, request, *args, **kwargs):
-        return Response({"temp": 1})
+        product = Product.objects.all().order_by('-id')
+
+        ret = []
+
+        for p in product:
+            ret.append({
+                'name': p.name,
+                'price': p.price,
+                'product_type': p.product_type,
+            })
+            
+        
+        return Response(ret)
+
+class ProductDetailView(APIView):
+    def get(self, request, pk,*args, **kwargs) :
+        product = Product.objects.get(pk = pk)
+
+        ret = {
+            'name': product.name,
+            'price': product.price,
+            'product_type': product.product_type,
+        }
+        
+        return Response(ret)
