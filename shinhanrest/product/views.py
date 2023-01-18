8221afce2d332f302b140209c303dcc4212f063a
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-18 14:43:07 +0900

    feat: Complete Read and Create

--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product
@@ -9,12 +10,18 @@ class ProductListView(APIView):
 
     def post(self, request, *args, **kwargs):
         product = Product(
+            # name = request.data.get('name),
             name = request.data['name'],
             price = request.data['price'],
             product_type = request.data['product_type'],
         )
-        product.save()
-        return Response()
+        product.save() # db에 저장 - 이 때 primary key가 만들어짐
+        return Response({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'product_type': product.product_type,
+        }, status=status.HTTP_201_CREATED)
 
     def get(self, request, *args, **kwargs):
         product = Product.objects.all().order_by('-id')
@@ -32,10 +39,14 @@ class ProductListView(APIView):
         return Response(ret)
 
 class ProductDetailView(APIView):
+    def delete(self, request, pk, *args, **kwargs):
+        pass
+
     def get(self, request, pk,*args, **kwargs) :
         product = Product.objects.get(pk = pk)
 
         ret = {
+            'id': product.id,
             'name': product.name,
             'price': product.price,
             'product_type': product.product_type,
