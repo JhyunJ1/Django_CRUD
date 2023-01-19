6291bf3940f2a4f9fd8da0522c20211bfe7721d8
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-19 10:00:45 +0900

    feat: add get_queryset feature

--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -2,14 +2,19 @@ from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListView(mixins.ListModelMixin, generics.GenericAPIView):
+class ProductListView(
+        mixins.ListModelMixin,
+        mixins.CreateModelMixin,
+        generics.GenericAPIView,
+    ):
+
     serializer_class = ProductSerializer
+
     def get_queryset(self):
-        return Product.objects.all().order_by('id')
-    
+        
 
     def get(self, request, *args, **kwargs):
-        # Queryset
-        # Serialize
-        # return Response
         return self.list(request, args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
